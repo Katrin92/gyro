5e253b006b2723c92f07904362131b97458281be
Stefan Noack <noackstefan@googlemail.com>
2011-11-04 18:44:46 +0100

    minor cosmetics

--- testlevels/simple_bridge.py
+++ testlevels/simple_bridge.py
@@ -8,7 +8,7 @@ class SimpleBridgeLevel(Level):
         self._init_plates()
     
     def _init_plates(self):
-        self.objects = (Plate(self, -10, 0, -0.5,  4, 4,  1,  1),        
+        self.objects = (Plate(self, -10, 0, -0.5,  4,   4,  1,  1),        
                         Plate(self,   0, 0, -1.5, 16, 2.5, .3,  1),
-                        Plate(self,  10, 0, -2.5,  4, 4, .5, .5))
+                        Plate(self,  10, 0, -2.5,  4,   4, .5, .5))
 
